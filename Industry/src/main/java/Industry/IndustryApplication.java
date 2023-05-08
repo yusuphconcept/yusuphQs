eb10f774f50e93cb81e5baaef4d973c4a312ab6f
yusuphconcept <77440340+yusuphconcept@users.noreply.github.com>
2023-05-08 11:28:34 -0700

    Add files via upload

---
+++ Industry/src/main/java/Industry/IndustryApplication.java
@@ -0,0 +1,13 @@
+package Industry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryApplication.class, args);
+	}
+
+}
---
+++ Industry/src/test/java/Industry/IndustryApplicationTests.java
@@ -0,0 +1,13 @@
+package Industry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
